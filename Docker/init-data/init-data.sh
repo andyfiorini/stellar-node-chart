8175cf87bd0004e5254580e2b584708fd1978007
Andres Lucas Garcia Fiorini <andres.fiorini@gmail.com>
2018-08-14 12:35:14 -0300

    different docker images

--- Docker/init-data/init-data.sh
+++ Docker/init-data/init-data.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "cd /opt/stellar"
 cd /opt/stellar
+mkdir /var/run/postgres
+chmod 777 /run /var/run /var/run/postgres
 [[ $? != 0 ]] && exit 230;
 echo "tar xvfj ..."
 tar xvfj /tmp/data.tar.bz2
